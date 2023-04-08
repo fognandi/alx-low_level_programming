bbe997c576ce73cf0a3acb9252809b3b778acc0e
Nandi Peterfog <peterfog5524@gmail.com>
2023-04-08 15:20:30 -0700

    1-args.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,11 +5,10 @@
   *main - print the name of the program
   *@argc: number of arguments
   *@argv: array of arguments
-  *
   *Return: ALways 0
   */
 
-int main(int argc__attribute__((unused)), char *argv[])
+int main(int argc__attribute__((unused)); char *argv[])
 {
 	printf("%s\n", *argv);
 
